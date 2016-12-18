e0164e7310694c01f3cdb702f2e611b8c8db8ba7
Bruno Coelho <4brunu@users.noreply.github.com>
2016-12-18 19:22:47 +0000

    improve test coverage

--- spec/git_status_action_spec.rb
+++ spec/git_status_action_spec.rb
@@ -3,7 +3,11 @@ describe Fastlane::Actions::GitStatusAction do
     it 'prints a message' do
       expect(Fastlane::UI).to receive(:message).with("The git_status plugin is working!")
 
-      Fastlane::Actions::GitStatusAction.run(nil)
+      Fastlane::Actions::GitStatusAction.run()
+      
+      Fastlane::Actions::GitStatusAction.run("./")
+      
+      Fastlane::Actions::GitStatusAction.run(["../", "./"])
     end
   end
 end
