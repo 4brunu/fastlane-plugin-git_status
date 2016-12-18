f54a36e60158406443c15c67e1a2f5161d0f1967
Bruno Coelho <4brunu@users.noreply.github.com>
2016-12-18 20:03:41 +0000

    improve documentation

--- spec/git_status_action_spec.rb
+++ spec/git_status_action_spec.rb
@@ -2,18 +2,18 @@ describe Fastlane::Actions::GitStatusAction do
   describe '#run' do
     
     it "call action without parameters" do
-      result = Fastlane::Actions::GitStatusAction.run(path: nil)
-      expect(result).to eq("")
+      result = Fastlane::Actions::GitStatusAction(path: nil)
+      expect(result).to eq("git status --porcelain .")
     end
     
     it "call action with a string parameter" do
       result = Fastlane::Actions::GitStatusAction.run(path: "./")
-      expect(result).to eq("")
+      expect(result).to eq("git status --porcelain ./")
     end
     
     it "call action with an array parameter" do
       result = Fastlane::Actions::GitStatusAction.run(path: ["../", "./"])
-      expect(result).to eq("")
+      expect(result).to eq("git status --porcelain ../ ./")
     end
     
   end
